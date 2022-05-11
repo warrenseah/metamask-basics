83bb6cfb4eac274f1078379f4753dea29630d2b1
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-05-11 16:57:48 +0800

    write metamask basic tutorial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import MetaMaskOnboarding from '@metamask/onboarding';
+import detectEthereumProvider from '@metamask/detect-provider';
 
 function App() {
+  const [metamask, setMetamask] = useState();
+  const [account, setAccount] = useState('');
+
+  useEffect(() => {
+    isMetamaskInstalled();
+    return () => {
+      window.ethereum.removeListener('accountsChanged', handleAccountsChange);
+      window.ethereum.removeListener('chainChanged', handleBlockchainChange);
+    }
+  },[]);
+
+  const isMetamaskInstalled = async () => {
+    const provider = await detectEthereumProvider();
+    if(provider) {
+      setMetamask(provider);
+      provider.on('accountsChanged', handleAccountsChange);
+      provider.on('chainChanged', handleBlockchainChange);
+    } else {
+      console.log('Please install MetaMask');
+    }
+  }
+
+  const handleAccountsChange = (accounts) => {
+		setAccount(accounts[0]);
+    console.log('Account changed');
+	}
+
+	const handleBlockchainChange = async (chainId) => {
+		// reload page
+    console.log('Chain ID changed');
+    window.location.reload();
+	}
+
+  const loadAccount = async () => {
+    try {
+      if (metamask) {
+        const accounts = await metamask.request({ method: 'eth_requestAccounts' });
+        console.log('Account loaded: ', accounts[0]);
+        setAccount(accounts[0]);
+      } else {
+        console.log('No metamask found');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const installMetamask = () => {
+    const forwarderOrigin = 'http://localhost:9010';
+    //We create a new MetaMask onboarding object to use in our app
+    const onboarding = new MetaMaskOnboarding({ forwarderOrigin });
+    onboarding.startOnboarding();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {metamask ? (<button onClick={isMetamaskInstalled}>Connected</button>) : (
+          <>
+            <button onClick={isMetamaskInstalled}>Connect</button>
+            <button onClick={installMetamask}>Click here to install MetaMask!</button>
+          </>
+        )}
+        {metamask ? (<button onClick={loadAccount}>Load account</button>) : null}
+        <p>Account: {account}</p>
       </header>
     </div>
   );
